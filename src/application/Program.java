e9db6a5a1502cb1250e7f5c555d2542943d75202
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-26 18:04:33 -0300

    FEAT: add Seller class and update main program to include Seller instance

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department("Books", 1);
-        System.out.println(obj);
+
+        Seller seller = new Seller(1, "Bob", "bob@gamil.com", 3000.0, new Date(), obj);
+        System.out.println(seller);
     }
 }
