114f6bf7ce29cb0f1e03eccc6de577f465665dd0
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-26 18:51:08 -0300

    TEST: retrieve seller details by ID in Program class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,19 +2,14 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department("Books", 1);
-
-        Seller seller = new Seller(1, "Bob", "bob@gamil.com", 3000.0, new Date(), obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
