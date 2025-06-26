5752b35dd63500801e6287bd2899e575acbf39a4
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-26 17:58:41 -0300

    FEAT: add Department class and main program

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Department obj = new Department("Books", 1);
+        System.out.println(obj);
+    }
+}
