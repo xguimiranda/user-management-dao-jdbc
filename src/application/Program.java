944c3cb3031140fffef18c9388145a07fed3c966
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-26 20:13:20 -0300

    FEAT: update console output for seller insertion and add seller update test in Program class

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,10 +28,16 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("\n === TEST 4: seller findAll ===");
+        System.out.println("\n === TEST 4: seller insert ===");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", 4000.0, new java.util.Date(), new Department(null, 2));
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n === TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Updated completed");
 
     }
 }
